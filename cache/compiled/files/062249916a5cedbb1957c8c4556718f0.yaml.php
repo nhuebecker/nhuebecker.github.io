af186e53183e22a797f072b0b5ea12e2587a55f1
nhuebecker <nikolas.huebecker@gmail.com>
2015-11-19 21:31:11 -0500

    Update

--- cache/compiled/files/062249916a5cedbb1957c8c4556718f0.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'plugins://form/form.yaml',
-    'modified' => 1446962992,
-    'data' => [
-        'enabled' => true
-    ]
-];
--- cache/compiled/files/2085fa4f950e7838ea9fac4a6da31813.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'user/plugins/form/form.yaml',
-    'modified' => 1446962992,
-    'data' => [
-        'enabled' => true
-    ]
-];
